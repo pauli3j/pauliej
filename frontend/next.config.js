87a5945d5c099d7c23f335873457713777d29502
Paulie J <pauliejes@me.com>
2024-05-31 09:31:56 -0400

    Newdawn Newday

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,7 +13,7 @@ module.exports = withTM({
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace "*" with your actual origin if needed
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
